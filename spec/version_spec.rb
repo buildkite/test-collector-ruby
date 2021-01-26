1118bfdb7dc7d95f562683daa20040a46e35d6cd
Matthew Draper <matthew@trebex.net>
2021-01-27 01:54:55 +1030

    Initial `bundle gem`

---
+++ spec/version_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RSpec::Buildkite::Insights do
+  it "has a version number" do
+    expect(RSpec::Buildkite::Insights::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
