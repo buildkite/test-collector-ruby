c0102983d4c60d45ab64b060dcbeca6096acb52e
Juanito Fatas <juanito@buildkite.com>
2021-04-13 21:19:50 +0900

    Compare ID is enough
    The unique id of this example. Pass this at the command line to re-run this exact example.
    
    https://github.com/rspec/rspec-core/blob/ea8554afd1a2b63677c6593059fa8f2476181deb/lib/rspec/core/example.rb#L115-L119

--- lib/rspec/buildkite/insights/reporter.rb
+++ lib/rspec/buildkite/insights/reporter.rb
@@ -9,7 +9,7 @@ module RSpec::Buildkite::Insights
     def handle_example(notification)
       example = notification.example
       trace = RSpec::Buildkite::Insights.uploader.traces.find do |trace|
-        compare_example(example, trace.example)
+        example.id == trace.example.id
       end
 
       if trace
@@ -21,13 +21,5 @@ module RSpec::Buildkite::Insights
     alias_method :example_passed, :handle_example
     alias_method :example_failed, :handle_example
     alias_method :example_pending, :handle_example
-
-    private
-
-    def compare_example(example, another_example)
-      example.file_path == another_example.file_path &&
-      example.full_description == another_example.full_description &&
-      example.location == another_example.location
-    end
   end
 end
