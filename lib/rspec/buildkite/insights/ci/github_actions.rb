1adf0e3a31f204d5b3ea5321b3bdec201b69a5c4
Juanito Fatas <juanito@buildkite.com>
2021-05-14 13:33:50 +0900

    Capture GitHub Actions ENVs

--- lib/rspec/buildkite/insights/ci/github_actions.rb
+++ lib/rspec/buildkite/insights/ci/github_actions.rb
@@ -2,13 +2,19 @@
 
 module RSpec::Buildkite::Insights::CI
   class GitHubActions
-    def self.key
-      File.join(
-        "https://github.com",
-        ENV["GITHUB_REPOSITORY"],
-        "actions/runs",
-        ENV["GITHUB_RUN_ID"]
-      )
+    NAME = "github_actions"
+
+    def self.env
+      key = ENV["GITHUB_ACTION"] + "-" + ENV["GITHUB_RUN_ID"] + "-" + ENV["GITHUB_RUN_NUMBER"]
+      url = File.join("https://github.com", ENV["GITHUB_REPOSITORY"], "actions/runs", ENV["GITHUB_RUN_ID"])
+
+      {
+        "CI" => NAME,
+        "key" => key,
+        "url" => url,
+        "branch" => ENV["GITHUB_REF"], # could be nil
+        "commit_sha" => ENV["GITHUB_SHA"],
+      }
     end
   end
 end
--- spec/insights/ci/github_actions_spec.rb
+++ spec/insights/ci/github_actions_spec.rb
@@ -3,24 +3,39 @@
 require "rspec/buildkite/insights/ci"
 
 RSpec.describe RSpec::Buildkite::Insights::CI::GitHubActions do
-  describe ".key" do
+  describe ".env" do
     before do
       allow(ENV).to receive(:[]).and_call_original
     end
 
     context "GitHub Actions" do
-      let(:build_url) { "https://github.com/buildkite/buildkite/actions/runs/1234" }
+      let(:action_id) { "161335" }
+      let(:run_id) { "840583718" }
+      let(:run_num) { "2" }
+      let(:repository) { "buildkite/buildkite" }
+      let(:branch) { "refs/heads/feature-branch-1" }
+      let(:commit_sha) { "9683a9a92ec0f3055849cd5488e8e9347c6e2878" }
 
       before do
         fake_env("GITHUB_ACTIONS", "true")
-        fake_env("GITHUB_REPOSITORY", "buildkite/buildkite")
-        fake_env("GITHUB_RUN_ID", "1234")
+        fake_env("GITHUB_ACTION", action_id)
+        fake_env("GITHUB_RUN_ID", run_id)
+        fake_env("GITHUB_RUN_NUMBER", run_num)
+        fake_env("GITHUB_REPOSITORY", repository)
+        fake_env("GITHUB_REF", branch)
+        fake_env("GITHUB_SHA", commit_sha)
       end
 
-      it "returns build url" do
-        result = RSpec::Buildkite::Insights::CI.key
+      it "returns env" do
+        result = RSpec::Buildkite::Insights::CI.env
 
-        expect(result).to eq build_url
+        expect(result).to match({
+          "CI" => "github_actions",
+          "key" => "161335-840583718-2",
+          "url" => "https://github.com/buildkite/buildkite/actions/runs/840583718",
+          "branch" => branch,
+          "commit_sha" => commit_sha,
+        })
       end
     end
   end
