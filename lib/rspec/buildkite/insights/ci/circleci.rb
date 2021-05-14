d98c3f223a3da90e5c25899ea632411aeb2e03a0
Juanito Fatas <juanito@buildkite.com>
2021-05-14 13:19:14 +0900

    Capture CircleCI ENVs

--- lib/rspec/buildkite/insights/ci/circleci.rb
+++ lib/rspec/buildkite/insights/ci/circleci.rb
@@ -2,8 +2,17 @@
 
 module RSpec::Buildkite::Insights::CI
   class CircleCI
-    def self.key
-      ENV["CIRCLE_BUILD_URL"]
+    NAME = "circleci"
+
+    def self.env
+      key = ENV["CIRCLE_WORKFLOW_ID"] + "-" + ENV["CIRCLE_BUILD_NUM"]
+      {
+        "CI" => NAME,
+        "key" => key,
+        "url" => ENV["CIRCLE_BUILD_URL"],
+        "branch" => ENV["CIRCLE_BRANCH"],
+        "commit_sha" => ENV["CIRCLE_SHA1"],
+      }
     end
   end
 end
--- spec/insights/ci/circleci_spec.rb
+++ spec/insights/ci/circleci_spec.rb
@@ -3,23 +3,38 @@
 require "rspec/buildkite/insights/ci"
 
 RSpec.describe RSpec::Buildkite::Insights::CI::CircleCI do
-  describe ".key" do
+  describe ".env" do
     before do
       allow(ENV).to receive(:[]).and_call_original
     end
 
-    context "CircleCI" do
+    context "Circle CI" do
+      let(:workflow_id) { "66a7325d-f222-41d5-a257-cc83bc8e6843" }
+      let(:build_num) { "12345" }
       let(:build_url) { "https://circleci.com/gh/circleci/frontend/1234" }
+      let(:branch) { "main" }
+      let(:commit_sha) { "9883a9a92ec0f3055849cd5488e8e9347c6e2878" }
 
       before do
+
         fake_env("CIRCLECI", "true")
+        fake_env("CIRCLE_WORKFLOW_ID", workflow_id)
+        fake_env("CIRCLE_BUILD_NUM", build_num)
         fake_env("CIRCLE_BUILD_URL", build_url)
+        fake_env("CIRCLE_BRANCH", branch)
+        fake_env("CIRCLE_SHA1", commit_sha)
       end
 
-      it "returns build url" do
-        result = RSpec::Buildkite::Insights::CI.key
+      it "returns env" do
+        result = RSpec::Buildkite::Insights::CI.env
 
-        expect(result).to eq build_url
+        expect(result).to match({
+          "CI" => "circleci",
+          "key" => "66a7325d-f222-41d5-a257-cc83bc8e6843-12345",
+          "url" => build_url,
+          "branch" => branch,
+          "commit_sha" => commit_sha,
+        })
       end
     end
   end
