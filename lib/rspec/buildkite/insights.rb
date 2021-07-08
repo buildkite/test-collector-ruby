8185e00e328986bb7c35216ed7e090e3ec001b64
Juanito Fatas <juanito@buildkite.com>
2021-07-08 15:23:55 +0900

    Add a connection_timeout config, defaults to 30s

--- lib/rspec/buildkite/insights.rb
+++ lib/rspec/buildkite/insights.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require "timeout"
+
 require_relative "insights/version"
 
 module RSpec::Buildkite::Insights
   class Error < StandardError; end
+  class TimeoutError < ::Timeout::Error; end
 
   DEFAULT_URL = "https://insights-api.buildkite.com/v1/uploads"
 
@@ -13,12 +16,14 @@ module RSpec::Buildkite::Insights
     attr_accessor :url
     attr_accessor :uploader
     attr_accessor :session
+    attr_accessor :connection_timeout
   end
 
-  def self.configure(token: nil, url: nil, filename: nil)
+  def self.configure(token: nil, url: nil, filename: nil, connection_timeout: nil)
     self.api_token = token || ENV["BUILDKITE_INSIGHTS_TOKEN"]
     self.url = url || DEFAULT_URL
     self.filename = filename
+    self.connection_timeout = connection_timeout || 30
 
     require_relative "insights/uploader"
 
