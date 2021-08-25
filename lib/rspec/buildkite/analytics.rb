d1fef2e4418deee6fa4865b57e9501c5d106efc5
niceking <rose@buildkite.com>
2021-08-25 18:05:48 +1200

    remove filename as config param

--- lib/rspec/buildkite/analytics.rb
+++ lib/rspec/buildkite/analytics.rb
@@ -12,16 +12,14 @@ module RSpec::Buildkite::Analytics
 
   class << self
     attr_accessor :api_token
-    attr_accessor :filename
     attr_accessor :url
     attr_accessor :uploader
     attr_accessor :session
   end
 
-  def self.configure(token: nil, url: nil, filename: nil)
+  def self.configure(token: nil, url: nil)
     self.api_token = token || ENV["BUILDKITE_ANALYTICS_TOKEN"]
     self.url = url || DEFAULT_URL
-    self.filename = filename
 
     require_relative "analytics/uploader"
 
--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe RSpec::Buildkite::Analytics do
-  it "can configure api_token, url, and filename" do
+  it "can configure api_token and url" do
     analytics = RSpec::Buildkite::Analytics
     ENV["BUILDKITE_ANALYTICS_TOKEN"] = "MyToken"
 
@@ -9,6 +9,5 @@ RSpec.describe RSpec::Buildkite::Analytics do
 
     expect(analytics.api_token).to eq "MyToken"
     expect(analytics.url).to eq "https://analytics-api.buildkite.com/v1/uploads"
-    expect(analytics.filename).to be nil
   end
 end
