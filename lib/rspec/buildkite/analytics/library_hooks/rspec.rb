e81f30ac48f30e2afe19af221c94e7efb13f4071
Mario Visic <mario@mariovisic.com>
2022-03-23 12:01:21 +0800

    Move the bulk of RSpec initialization to the Uploader class

--- lib/rspec/buildkite/analytics/library_hooks/rspec.rb
+++ lib/rspec/buildkite/analytics/library_hooks/rspec.rb
@@ -9,49 +9,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     config.add_formatter RSpec::Buildkite::Analytics::Reporter
 
-    if RSpec::Buildkite::Analytics.api_token
-      contact_uri = URI.parse(RSpec::Buildkite::Analytics.url)
-
-      http = Net::HTTP.new(contact_uri.host, contact_uri.port)
-      http.use_ssl = contact_uri.scheme == "https"
-
-      authorization_header = "Token token=\"#{RSpec::Buildkite::Analytics.api_token}\""
-
-      contact = Net::HTTP::Post.new(contact_uri.path, {
-        "Authorization" => authorization_header,
-        "Content-Type" => "application/json",
-      })
-      contact.body = {
-        run_env: RSpec::Buildkite::Analytics::CI.env,
-        format: "websocket"
-      }.to_json
-
-      response = begin
-        http.request(contact)
-      rescue *RSpec::Buildkite::Analytics::REQUEST_EXCEPTIONS => e
-        puts "Buildkite Test Analytics: Error communicating with the server: #{e.message}"
-      end
-
-      return unless response
-
-      case response.code
-      when "401"
-        puts "Buildkite Test Analytics: Invalid Suite API key. Please double check your Suite API key."
-      when "200"
-        json = JSON.parse(response.body)
-
-        if (socket_url = json["cable"]) && (channel = json["channel"])
-          RSpec::Buildkite::Analytics.session = RSpec::Buildkite::Analytics::Session.new(socket_url, authorization_header, channel)
-        end
-      else
-        request_id = response.to_hash["x-request-id"]
-        puts "rspec-buildkite-analytics could not establish an initial connection with Buildkite. You may be missing some data for this test suite, please contact support."
-      end
-    else
-      if !!ENV["BUILDKITE_BUILD_ID"]
-        puts "Buildkite Test Analytics: No Suite API key provided. You can get the API key from your Suite settings page."
-      end
-    end
+    RSpec::Buildkite::Analytics::Uploader.configure
   end
 
   config.around(:each) do |example|
