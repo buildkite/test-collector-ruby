09fe36148d62663cf573ac00bbea5577d9afe0f8
Mario Visic <mario@mariovisic.com>
2022-04-03 10:31:19 +1000

    Replace invalid UTF-8 characters in test names
    In the `Session` class we convert our test results into a JSON string to send
    over the wire. Calling `to_json` on a string that has invalid UTF-8 characters
    result in a `JSON::GeneratorError`. Which means that if a test name has any
    invalid characters we won't be able to send its results to analytics. So we
    replace any of these characters.
    
    I think it would have been good to avoid the recursive function here and
    only re-encoded the strings that are generated from the users test
    suite, but that happens in quite a few places, it would be easy to skip
    doing this when adding additional data from the test suite as well; also
    those values appearing in deep nested structures, so we can't just call
    that function for a few keys, we need to recursively visit each string
    in our hash.

--- lib/rspec/buildkite/analytics/uploader.rb
+++ lib/rspec/buildkite/analytics/uploader.rb
@@ -40,7 +40,7 @@ module RSpec::Buildkite::Analytics
       # FIXME: RSpec specific, maybe create a different uploader for minitest?
       # Or just extract this part out to a seperate class ?
       def as_hash
-        {
+        strip_invalid_utf8_chars(
           id: @id,
           scope: example.example_group.metadata[:full_description],
           name: example.description,
@@ -51,14 +51,14 @@ module RSpec::Buildkite::Analytics
           failure_reason: failure_reason,
           failure_expanded: failure_expanded,
           history: history,
-        }.with_indifferent_access.compact
+        )
       end
 
       private
 
       def generate_file_name(example)
         file_path_regex = /^(.*?\.(rb|feature))/
-        identifier_file_name = example.id[file_path_regex]
+        identifier_file_name = strip_invalid_utf8_chars(example.id)[file_path_regex]
         location_file_name = example.location[file_path_regex]
 
         if identifier_file_name != location_file_name
@@ -74,6 +74,16 @@ module RSpec::Buildkite::Analytics
           end
         else
           identifier_file_name
+        end
+      end
+
+      def strip_invalid_utf8_chars(object)
+        if(object.is_a?(Hash))
+          Hash[object.map { |key, value| [key, strip_invalid_utf8_chars(value)] }]
+        elsif object.is_a?(Array)
+          object.map { |value| strip_invalid_utf8_chars(value) }
+        elsif object.is_a?(String)
+          object.encode('UTF-8', :invalid => :replace, :undef => :replace)
         end
       end
     end
