f328372a56234384b7bf19c7758d34ad6a85637a
Juanito Fatas <me@juanitofatas.com>
2022-05-23 11:27:43 +0900

    Add a failing spec to test reporter’s failure code path without any NameError

---
+++ spec/collector/rspec_plugin/reporter_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require "buildkite/collector/rspec_plugin/reporter"
+require "buildkite/collector/uploader"
+
+RSpec.describe Buildkite::Collector::RSpecPlugin::Reporter do
+  class FakeExecutionResult
+    attr :started_at, :finished_at, :run_time, :status, :exception
+    def initialize(status: :passed, skipped: false, pending_fixed: false, exception: nil)
+      now = Time.now
+      @started_at = now
+      @finished_at = now + rand(5)
+      @run_time = finished_at - started_at
+      @status = status
+      @skipped = skipped
+      @pending_fixed = pending_fixed
+      @exception = status == :failed ? StandardError.new("fake error") : nil
+    end
+    def example_skipped?
+      @skipped
+    end
+    def pending_fixed?
+      @pending_fixed
+    end
+  end
+
+  it "test reporter works with a failed RSpec example" do
+    Buildkite::Collector.configure(
+      token: "fake",
+      url: "http://fake.buildkite.localhost/v1/uploads",
+    )
+    io = StringIO.new
+    reporter = Buildkite::Collector::RSpecPlugin::Reporter.new(io)
+    example = double("RSpec::Core::Example")
+    allow(example).to receive(:execution_result) { FakeExecutionResult.new(status: :failed) }
+    allow(example).to receive(:id) { "spec/fake/fake_spec[1:2:3]" }
+    allow(example).to receive(:full_description) { "this is a fake error full description" }
+    allow(example).to receive(:metadata) { Hash.new(shared_group_inclusion_backtrace: []) }
+    fake_trace = double("Buildkite::Collector::RSpecPlugin::Trace", example: example)
+    allow(fake_trace).to receive(:[]) { fake_trace }
+    allow(fake_trace).to receive(:example=)
+    allow(fake_trace).to receive(:failure_reason=)
+    allow(fake_trace).to receive(:failure_expanded=)
+    allow(Buildkite::Collector.uploader).to receive(:traces) { fake_trace }
+
+    notification = RSpec::Core::Notifications::ExampleNotification.for(example)
+    allow(notification).to receive(:colorized_message_lines) { [""] }
+
+    reporter.handle_example(notification)
+
+    reset_io(io)
+  end
+end
