3e0c4eb4b810f207274477a5fc03f71670fd384f
Christian Bruckmayer <christian.bruckmayer@shopify.com>
2022-06-20 08:58:01 +0100

    Fix Minitest crashing without loading minitest lib hook
    The library hooks are loaded as part of the configure call.
    However, we don't always want to run test analytics and therefore
    only call configure on certain pipelines, builds etc.
    As Minitest automatically discovers plugins, this will crash with a
    NameError.

--- lib/minitest/buildkite_collector_plugin.rb
+++ lib/minitest/buildkite_collector_plugin.rb
@@ -1,6 +1,6 @@
 module Minitest
   def self.plugin_buildkite_collector_init(options)
-    if Buildkite::TestCollector.respond_to?(:uploader)
+    if Buildkite::TestCollector.respond_to?(:api_token) && Buildkite::TestCollector.api_token
       self.reporter << Buildkite::TestCollector::MinitestPlugin::Reporter.new(options[:io], options)
     end
   end
