c482f62ba16082038076e9a87609ac5a76c85eca
Naufan Rizal <naufan@buildkite.com>
2023-01-27 15:23:18 +1300

    add proper test

--- spec/test_collector/minitest_plugin/trace_spec.rb
+++ spec/test_collector/minitest_plugin/trace_spec.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 require "buildkite/test_collector/minitest_plugin/trace"
+require "minitest"
 
 RSpec.describe Buildkite::TestCollector::MinitestPlugin::Trace do
-  subject(:trace) { Buildkite::TestCollector::MinitestPlugin::Trace.new(result, history: history) }
-  let(:result) { double("Result", name: "test_it_passes", test_it_passes: nil, result_code: 'F', failure: failure, failures: [failure]) }
-  let(:failure) { double("Failure", message: message, backtrace: backtrace)}
-  let(:message) { "test for invalid character'\xC8'\n    Expected: true\n    Actual: false" }
+  subject(:trace) { Buildkite::TestCollector::MinitestPlugin::Trace.new(example, history: history) }
+  let(:example) { double("Minitest::Test", name: "test_it_passes", test_it_passes: nil, result_code: 'F', failure: failure, failures: [failure, another_failure]) }
+
+  # failure is either Minitest::Assertion or Minitest::UnexpectedError object. 
+  # ref: https://github.com/minitest/minitest/blob/master/lib/minitest/test.rb#L198
+  let(:failure) { instance_double(Minitest::Assertion, message: "test for invalid character'\xC8'\n    Expected: true\n    Actual: false", backtrace: backtrace) }
+  let(:another_failure) { instance_double(Minitest::Assertion, message: "another test\n    Expected thing to be truthy.", backtrace: backtrace) }
+
   let(:backtrace) { [
     '# ./lib/test/test.rb:5:in',
     '# ./lib/test/test.rb:15:in',
@@ -53,15 +58,25 @@ RSpec.describe Buildkite::TestCollector::MinitestPlugin::Trace do
     end
 
     describe "failure_expanded" do
-      it "is not empty" do
+      it "contains all failures" do
         failure_expanded = trace.as_hash[:failure_expanded]
-        expect(failure_expanded).not_to be_empty
+        expect(failure_expanded.count).to eq(2)
       end
 
-      it "contains the remaining lines of failure message" do
-        failure_expanded = trace.as_hash[:failure_expanded][0][:expanded].to_s
-        expect(failure_expanded).not_to include("test for invalid character")
-        expect(failure_expanded).to include("Expected: true", "Actual: false")
+      it "contains expanded message and backtrace for each failure" do
+        failure_expanded = trace.as_hash[:failure_expanded]
+        expect(failure_expanded).to all( include(:expanded, backtrace: backtrace) )
+      end
+
+      it "does not contain the first line of failure message for first failure" do
+        first_failure = trace.as_hash[:failure_expanded][0][:expanded].to_s
+        expect(first_failure).not_to include("test for invalid character")
+        expect(first_failure).to include("Expected: true", "Actual: false")
+      end
+
+      it "contains the all lines of failure message for the other failures" do
+        another_failure = trace.as_hash[:failure_expanded][1][:expanded].to_s
+        expect(another_failure).to include("another test","Expected thing to be truthy.")
       end
     end
 
