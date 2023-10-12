4bceef48f517674e9873926a13bb8dff2f4977f5
Meghan Kradolfer <meghan@buildkite.com>
2023-10-12 16:45:40 +1300

    Error handling around the response

--- lib/buildkite/test_collector/test_links_plugin/reporter.rb
+++ lib/buildkite/test_collector/test_links_plugin/reporter.rb
@@ -23,19 +23,24 @@ module Buildkite::TestCollector::TestLinksPlugin
     def dump_summary(_notification)
       return false unless Buildkite::TestCollector.api_token
 
-      response = Buildkite::TestCollector::Uploader.response
-      res = JSON.parse(response.body)
+      begin
+        response = Buildkite::TestCollector::Uploader.response
+        res = JSON.parse(response.body)
+        suite_url = res['suite_url']
+      rescue StandardError => e
+        @output.puts 'Error: cannot find test suite'
+        retun
+      end
 
-      @output.puts "\n\nTest Analytics failures:\n"
-
+      @output.puts "\n\nTest Analytics failures:\n\n"
       @failed_examples.each do |example|
         scope_name_digest = generate_scope_name_digest(example)
-        url = res["suite_url"] + "/tests/#{scope_name_digest}"
-        content = "#{example[:scope]} #{example[:name]}"
-
-        @output.puts "\e]1339;url=#{url};content=#{content}\a"
+        url = suite_url + "/tests/#{scope_name_digest}?scope_name_digest=true"
+        @output.puts "#{example[:scope]} #{example[:name]} \x1b]1339;url=#{url};content='View in Test Analytics'\a"
       end
     end
+
+    private
 
     def generate_scope_name_digest(result)
       Digest::SHA256.hexdigest(result[:scope].to_s + result[:name].to_s)
