39bebfcd767315c9487d823f63a5436ba02230d8
Paul Annesley <paul@annesley.cc>
2024-03-06 19:24:28 +1030

    test_collector_spec: test BUILDKITE_ANALYTICS_TRACE_MIN_MS env

--- spec/test_collector_spec.rb
+++ spec/test_collector_spec.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe Buildkite::TestCollector do
+  # Perhaps there's a better way to make a stubbed ENV overlay that resets between tests.
+  # We could probably use allow(ENV).to receive(...) although I find that more fragile.
+  # Also, I hadn't seen spec/support/fake_env_helpers.rb when I wrote this :|
+  ENV_REAL = ENV
+  let(:env_overlay) { Hash.new { |_h, k| ENV_REAL[k] } }
+  before { stub_const("ENV", env_overlay) }
+
   context "RSpec" do
     let(:hook) { :rspec }
 
     it "can configure api_token and url" do
       analytics = Buildkite::TestCollector
-      ENV["BUILDKITE_ANALYTICS_TOKEN"] = "MyToken"
+      env_overlay["BUILDKITE_ANALYTICS_TOKEN"] = "MyToken"
 
       analytics.configure(hook: hook)
 
@@ -22,6 +29,19 @@ RSpec.describe Buildkite::TestCollector do
 
       expect(analytics.env).to match env
     end
+
+    it "can configure (and unconfigure) trace_min_duration" do
+      Buildkite::TestCollector.configure(hook: hook)
+      expect(Buildkite::TestCollector.trace_min_duration).to eq(nil)
+
+      env_overlay["BUILDKITE_ANALYTICS_TRACE_MIN_MS"] = "123"
+      Buildkite::TestCollector.configure(hook: hook)
+      expect(Buildkite::TestCollector.trace_min_duration).to eq(0.123)
+
+      env_overlay.delete("BUILDKITE_ANALYTICS_TRACE_MIN_MS")
+      Buildkite::TestCollector.configure(hook: hook)
+      expect(Buildkite::TestCollector.trace_min_duration).to eq(nil)
+    end
   end
 
   context "Minitest" do
@@ -29,7 +49,7 @@ RSpec.describe Buildkite::TestCollector do
 
     it "can configure api_token and url" do
       analytics = Buildkite::TestCollector
-      ENV["BUILDKITE_ANALYTICS_TOKEN"] = "MyToken"
+      env_overlay["BUILDKITE_ANALYTICS_TOKEN"] = "MyToken"
 
       analytics.configure(hook: hook)
 
